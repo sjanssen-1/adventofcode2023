8f116c7dd80631ec1631848d5be480e3902cce41
sjanssen <janssensstef2000@gmail.com>
2023-12-26 12:01:42 +0100

    Day 18

--- day18/main.go
+++ day18/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"adventofcode2023/util"
+	"fmt"
 	"log"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -12,88 +14,122 @@ import (
 
 // https://adventofcode.com/2023/day/18
 func main() {
-	digInstructions := util.ScannerToStringSlice(*util.ReadFile("/Users/stefjanssens/git/adventofcode2023/day18/demo_input.txt"))
+	digInstructions := util.ScannerToStringSlice(*util.ReadFile("C:\\Users\\janss\\git\\adventofcode2023\\day18\\input.txt"))
 	defer util.TimeTrack(time.Now(), "main")
 
 	parsedDigInstructions := sliceutils.Map(digInstructions, func(value string, index int, slice []string) DigInstruction {
 		return NewDigInstruction(value)
 	})
 
+	parsedDigInstructions2 := sliceutils.Map(digInstructions, func(value string, index int, slice []string) DigInstruction {
+		return NewDigInstruction2(value)
+	})
+
 	log.Default().Printf("P1: %d", part1(parsedDigInstructions))
-	log.Default().Printf("P2: %d", part2())
+	log.Default().Printf("P2: %d", part2(parsedDigInstructions2))
 }
 
 type DigInstruction struct {
 	direction string
-	amount    float64
-	color     string
+	amount    int
 }
 
 type Point struct {
-	x, y float64
+	x, y int
 }
 
 func NewDigInstruction(digInstruction string) DigInstruction {
 	split := strings.Split(digInstruction, " ")
 	amount, _ := strconv.Atoi(split[1])
-	return DigInstruction{split[0], float64(amount), split[2]}
+	return DigInstruction{split[0], amount}
+}
+
+func NewDigInstruction2(digInstruction string) DigInstruction {
+	split := strings.Split(digInstruction, " ")
+	encoded := split[2]
+
+	amount, _ := strconv.ParseInt(encoded[2:len(encoded)-2], 16, 64)
+	switch encoded[len(encoded)-2 : len(encoded)-1] {
+	case "0":
+		return DigInstruction{"R", int(amount)}
+	case "1":
+		return DigInstruction{"D", int(amount)}
+	case "2":
+		return DigInstruction{"L", int(amount)}
+	case "3":
+		return DigInstruction{"U", int(amount)}
+	}
+	panic("Weird direction")
 }
 
 func calculateArea(digInstructions []DigInstruction) int {
 	vertices := make([]Point, 0)
-	currentX := 0.0
-	currentY := 0.0
+	currentX := 0
+	currentY := 0
+	boundary := 0
+
+	vertices = append(vertices, Point{0, 0})
 
 	for _, digInstruction := range digInstructions {
 		switch digInstruction.direction {
 		case "R":
-			for x := currentX; x < currentX+digInstruction.amount; x++ {
-				vertices = append(vertices, Point{x, currentY})
-			}
 			currentX += digInstruction.amount
 		case "L":
-			for x := currentX; x > currentX-digInstruction.amount; x-- {
-				vertices = append(vertices, Point{x, currentY})
-			}
 			currentX -= digInstruction.amount
 		case "U":
-			for y := currentY; y > currentY-digInstruction.amount; y-- {
-				vertices = append(vertices, Point{currentX, y})
-			}
 			currentY -= digInstruction.amount
 		case "D":
-			for y := currentY; y < currentY+digInstruction.amount; y++ {
-				vertices = append(vertices, Point{currentX, y})
-			}
 			currentY += digInstruction.amount
 		}
+		vertices = append(vertices, Point{currentX, currentY})
+		boundary += digInstruction.amount
 	}
 
-	return int(shoelace(vertices))
+	return picksTheorem(shoelace(vertices), boundary) + boundary
+}
+
+func debug(vertices []Point) {
+	i := 0
+	for y := -500; y < 500; y++ {
+		for x := -500; x < 500; x++ {
+			if slices.ContainsFunc(vertices, func(p Point) bool {
+				return p.x == x && p.y == y
+			}) {
+				i++
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println("")
+	}
+	fmt.Printf("Amount vs printed %d/%d", i, len(vertices))
 }
 
 // https://www.101computing.net/the-shoelace-algorithm/
-func shoelace(vertices []Point) float64 {
+func shoelace(vertices []Point) int {
 	n := len(vertices)
-	area := 0.0
+	area := 0
 
 	for i := 0; i < n; i++ {
 		j := (i + 1) % n
 		area += (vertices[i].x * vertices[j].y) - (vertices[j].x * vertices[i].y)
 	}
 
-	area = 0.5 * area
-	if area < 0 {
-		area = -area
-	}
+	return area / 2
+}
 
-	return area
+// https://en.wikipedia.org/wiki/Pick%27s_theorem
+func picksTheorem(area int, boundary int) int {
+	// A = i + (b/2) - 1
+	// i = A - (b/2) + 1
+	return area - (boundary / 2) + 1
 }
 
 func part1(digInstructions []DigInstruction) int {
 	return calculateArea(digInstructions)
 }
 
-func part2() int {
-	return 0
+func part2(digInstructions []DigInstruction) int {
+	return calculateArea(digInstructions)
 }
